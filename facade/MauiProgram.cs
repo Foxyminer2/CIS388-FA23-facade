4e3d056328b1c7862bf77b47680591f64f8c7d7d
Aapple21 <aapplegate12@icloud.com>
2023-09-13 23:09:03 -0500

    V2

--- facade/MauiProgram.cs
+++ facade/MauiProgram.cs
@@ -13,6 +13,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("VT323-Regular.ttf", "Pixel");
 			});
 		builder.Services.AddTransient<GameOverPage>();
 		
